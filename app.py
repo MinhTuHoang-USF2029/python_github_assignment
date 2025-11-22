9ba53227307b55b4ae1c945551347dcdc4ee234e
Minh Tu Hoang <tuhoang@usf.edu>
2025-11-22 14:49:08 -0500

    Clean up code, add comments, finalize program

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-
+# Welcome Message
 print("Welcome to my Python program!")
 
-
+# Input studying hours today
 hours = input("How many hours did you study today? ")
 
-
+# Convert input into float and calculate the estimated studying hours this week
 hours = float(hours)
 weekly_hours = hours * 7
 
-
+# Remind how many hours to study this week
 print(f"You are on track to study {weekly_hours} hours this week")
 
-
+# Handle incorrect user input's type error
 try:
     hours = float(hours)
 except ValueError:
